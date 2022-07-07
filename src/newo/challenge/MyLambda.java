ee22dae47a4a624a46a68518aea3fc486e089329
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-07-07 11:34:40 +0200

    finished with asked isOdd

--- src/newo/challenge/MyLambda.java
+++ src/newo/challenge/MyLambda.java
@@ -7,28 +7,48 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.stream.IntStream;
 
+@FunctionalInterface
+interface BooleanFunction{
+	boolean run(int n);
+}
+
 class MyLambda {
 
-	public boolean isOdd(int n) {
-		return (n%2 != 0 && n>0)? true: false;
-	}
 	
-	public String[] checker(int[] nums) {
+//	public boolean isOdd(int n) {
+//		return ;
+//	}
+//	
+	
+	BooleanFunction isOdd = (n) -> {return (n>0 && n%2 != 0)? false: true;};
+	BooleanFunction isPrime = (n) -> {
+		for (int i=2; i<n; i++) {
+			if (n%i == 0) {
+				return false;
+			}
+		}
+		return true;};
+	BooleanFunction isPalindrome = (n) -> {
+		String strNum = String.valueOf(n);
+		int numChars = strNum.length();
+		for (int i=0; i<numChars/2; i++) {
+			if (strNum.charAt(i) != strNum.charAt(numChars-i-1)) {
+				return false;
+			}
+		}
+		return true;};
+	
+	public List<List<String>> checker(List<Integer> nums) {
 		
 		
-		String[] result;
-		result.
-//		
-//		for (int i=0; i<nums.length; i++)
-//		{
-//			results.add()
-//		}
-//		
-		Arrays.stream(nums).forEach((p)->{
-			HashMap<String, String> res = new HashMap<String,String>();
-			res.put("input", String.valueOf(p));
-			result.
-		});
-		return res;
+		List<List<String>> result = new ArrayList<List<String>>();
+		nums.forEach((n) ->{ 
+			List<String> charac = new ArrayList<String>();
+			charac.add("input:"+String.valueOf(n));
+			charac.add("isOdd:"+String.valueOf(isOdd.run(n)));
+			charac.add("isPrime:"+String.valueOf(isPrime.run(n)));
+			charac.add("isPalindrome:"+String.valueOf(isPalindrome.run(n)));
+			result.add(charac);});
+		return result;
 	}
 }
--- src/newo/challenge/WelcomeToLambdas.java
+++ src/newo/challenge/WelcomeToLambdas.java
@@ -1,5 +1,18 @@
 package newo.challenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WelcomeToLambdas {
 
+	public static void main(String[] args) {
+		List<Integer> nums = new ArrayList<Integer>(Arrays.asList(4,5,898,34));
+		System.out.println(nums);
+		
+		MyLambda l = new MyLambda();
+		
+		System.out.println(l.checker(nums));
+		
+	}
 }
